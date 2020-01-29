c7a089175d3b011c7faff433c9a7ab7a0855ee24
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-01-28 22:55:37 -0600

    Preliminary attempt to make presenter code

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import re
 from nltk.corpus import state_union
 from nltk.tokenize import PunktSentenceTokenizer
 
-ignore = ['golden', 'globes', 'globe']
+ignore = ['golden', 'globes', 'globe', 'in', 'by', 'an', 'a', 'or', 'best', 'motion', 'picture', 'role', 'actor', 'actress', 'screenplay', 'comedy']
 
+# THIS LIST IS NOT RETURNED. IT IS USED TO FIND PRESENTERS, NOMINEES, AND WINNERS FOR ALL THE AWARDS IN ACCORDANCE WITH THE PROJECT GUIDELINES
+# WE ARE USING THIS LIST TO AVOID CASCADING ERROR
 OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
 
 award_template = {"Presenters": [], "Nominees": [], "Winner": None}
@@ -21,7 +23,7 @@ def process_tweets(filepath = "./data/gg2020.json"):
         for line in f:
             tweets.append(json.loads(str(line)))
 
-    # get_host(tweets)
+    get_host(tweets)
     # get_awards(tweets)
     get_presenters(tweets)
 
@@ -30,7 +32,6 @@ def get_host(tweets):
     host_plural = 0
     host_tweets = 0
     tknzr = nltk.tokenize.casual.TweetTokenizer(strip_handles=True)
-
     for tweet in tweets:
         text = tweet['text']
         if ' next ' in text:
@@ -47,13 +48,14 @@ def get_host(tweets):
                 first_word = tags[i][0]
                 sec_tag = tags[i+1][1] == "NNP"
                 sec_word = tags[i+1][0]
-                if first_tag and sec_tag and first_word not in ignore and sec_word not in ignore:
+                if first_tag and sec_tag and first_word.lower() not in ignore and sec_word.lower() not in ignore:
                     maybe_name = f"{first_word} {sec_word}"
-                    if maybe_name in host_count:
+                    if maybe_name in host_count.keys():
                         host_count[maybe_name] += 1
                     else:
-                        host_count[maybe_name] = 1
+                        host_count[maybe_name] = 0
     hosts = [k for k, _ in sorted(host_count.items(), key=lambda item: item[1], reverse=True)][:10]
+
     if host_plural/host_tweets < 0.5:
         print(hosts[:1])
     else:
@@ -78,27 +80,35 @@ def get_awards(tweets):
 
 def get_presenters(tweets):
     present_count = {k:{"presenters": {}, "present_plural": 0, "present_tweets": 0} for k in OFFICIAL_AWARDS_1819}
-    found_award = None
-
     tknzr = nltk.tokenize.casual.TweetTokenizer(strip_handles=True)
 
     for tweet in tweets:
         text = tweet['text']
+
+        best_score = float("-inf")
+        best_award = ""
+
         match = re.search(r'\(?((cecil)|(best)).*((award)|(drama)|(film)|(musical)|(motion picture)|(television)|(comedy))\)?', text.lower())
 
         if not match:
             continue
         
         for award in OFFICIAL_AWARDS_1819:
-            closest_award = None
-            best_score = float('-inf')
+            score = 0
+            award_key = ''.join(c for c in award if c.isalpha() or c==' ').split(' ')
 
-        # FIX THIS CODE PLS
+            for w in award_key:
+                if w in match.group():
+                    score += 1
+
+            if score > best_score:
+                best_score = score
+                best_award = award
 
         if ' present' in text:
-            present_count[found_award]["present_tweets"] += 1
+            present_count[best_award]["present_tweets"] += 1
             if ' and ' in text:
-                present_count[found_award]["present_plural"] += 1
+                present_count[best_award]["present_plural"] += 1
 
             tags = nltk.pos_tag(tknzr.tokenize(text))
             for i in range(len(tags)-1):
@@ -108,15 +118,15 @@ def get_presenters(tweets):
                 sec_word = tags[i+1][0]
                 if first_tag and sec_tag and first_word.lower() not in ignore and sec_word.lower() not in ignore:
                     maybe_name = f"{first_word} {sec_word}"
-                    if maybe_name in present_count[found_award]:
-                        present_count[found_award]["presenters"][maybe_name] += 1
+                    if maybe_name in present_count[best_award]["presenters"].keys():
+                        present_count[best_award]["presenters"][maybe_name] += 1
                     else:
-                        present_count[found_award]["presenters"][maybe_name] = 1
+                        present_count[best_award]["presenters"][maybe_name] = 1
 
     # for k in present_count.keys():
     #     present_count[k]["presenters"] = sorted(present_count[k]["presenters"].items(), key=lambda item: item[1], reverse=True)[:10]
 
-    print(present_count["best actor drama"]["presenters"])
+    # print(present_count)
 
         
 
