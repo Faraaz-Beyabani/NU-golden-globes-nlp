c0f7dddcb029b6bc9979a31b3ba70aa284a5b61f
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-01-31 13:23:35 -0600

    Cool formula for unigram and bigram selection

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import json
 import nltk
 import re
-from nltk.corpus import state_union
-from nltk.tokenize import PunktSentenceTokenizer
+
 from nltk import distance
 from collections import defaultdict
 
@@ -15,17 +14,25 @@ class GoldenGlobesParser:
     nominees = {}
     winners = {}
 
-    award_template = {"Presenters": [], "Nominees": [], "Winner": None}
+    
     ignore = ['golden', 'globes', 'globe', 'goldenglobes', '#goldenglobes']
         
     # THIS LIST IS NOT RETURNED. IT IS USED TO FIND PRESENTERS, NOMINEES, AND WINNERS FOR ALL THE AWARDS IN ACCORDANCE WITH THE PROJECT GUIDELINES
     # WE ARE USING THIS LIST TO AVOID CASCADING ERROR
     OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
+    OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
+    official_awards = []
+    
+    award_template = {"Presenters": [], "Nominees": [], "Winner": None}
     tweetized_awards = []
     categorized_winners = defaultdict(dict)
 
     def __init__(self, year = None):
         self.year = year or 2020
+        if self.year == 2013 or self.year == 2015:
+            self.official_awards = self.OFFICIAL_AWARDS_1315
+        else:
+            self.official_awards = self.OFFICIAL_AWARDS_1315
 
     def parse_json(self, file):
         tweets = []
@@ -58,6 +65,7 @@ class GoldenGlobesParser:
 
         self.process_awards()
         self.extract_winners()
+        
         # self.get_presenters(self.tweets)
 
     def process_awards(self):
@@ -83,74 +91,7 @@ class GoldenGlobesParser:
             curr = ' '.join([x for x in temp if x])
 
             self.tweetized_awards.append(curr)
-        # print(self.tweetized_awards)
-      
-    def extract_winners(self):
-        unigrams = defaultdict(dict)
-        tknzr = nltk.tokenize.casual.TweetTokenizer(strip_handles=True)
-        award_words = set(['tv', 'movie', 'wins', 'won'])
-        for award in self.OFFICIAL_AWARDS_1819:
-            for word in award.split():
-                award_words.add(word)
-
-        for award in self.tweetized_awards:
-            self.categorized_winners[award] = defaultdict(int)
-            unigrams[award] = defaultdict(int)
-            for tweet in self.tweets:
-                if self.match_award(tweet, award):
-                    if ('actor' in tweet['text'].lower() and 'actor' not in award) or ('actress' in tweet['text'].lower() and 'actress' not in award) or ('tv' in tweet['text'].lower() and 'tv' not in award):
-                        continue
-
-                    tags = nltk.pos_tag(tknzr.tokenize(tweet["text"]))
-                    for i in range(len(tags)-1):
-                        first_tag = tags[i][1] == "NNP"
-                        first_word = tags[i][0].lower()
-                        sec_tag = tags[i+1][1] == "NNP"
-                        sec_word = tags[i+1][0].lower()
-
-                        if first_word in award_words or sec_word in award_words:
-                            continue
-
-                        if first_tag and first_word not in self.ignore and sec_tag and sec_word not in self.ignore:
-                            maybe_name = f"{first_word} {sec_word}"
-                            self.categorized_winners[award][maybe_name] += 1
-                            unigrams[award][first_word] += 1
-                                
-        best_bn = None
-        best_bc = None
-        best_un = None
-        best_uc = None
-
-        for k,v in self.categorized_winners.items():
-            best_bn = float('-inf')
-            best_bc = None
-            for c, n in v.items():
-                # if c == "tom hanks":# or c == "hanks" or c == "tom":
-                #     print(f"{c}: {n}")
-                if n > best_bn:
-                    best_bn = n
-                    best_bc = c
-
-            best_un = float('-inf')
-            best_uc = None
-            for uc, un in unigrams[k].items():
-                if un > best_un:
-                    best_un = un
-                    best_uc = uc
-            
-            print(f"{best_bc} won {k} with {best_bn} occurrences")
-            print(f"{best_uc} won {k} with {best_un} occurrences")
-
         
-
-    def match_award(self, tweet, award):
-        for word in award.split():
-            if word not in tweet["text"].lower():
-                return False
-
-        return True
-        
-
     def extract_host(self, tweets):
         host_count = {}
         host_plural = 0
@@ -204,7 +145,78 @@ class GoldenGlobesParser:
                 self.awards.append(dic[key][0])
 
         print(self.awards)
-        # print(len(self.awards))
+
+    def extract_winners(self):
+        unigrams = defaultdict(dict)
+        tknzr = nltk.tokenize.casual.TweetTokenizer(strip_handles=True)
+        award_words = set(['tv', 'movie', 'wins', 'won', 'film', 'feature'])
+        for award in self.OFFICIAL_AWARDS_1819:
+            for word in award.split():
+                award_words.add(word)
+
+        for award in self.tweetized_awards:
+            self.categorized_winners[award] = defaultdict(int)
+            unigrams[award] = defaultdict(int)
+            for tweet in self.tweets:
+                if self.match_award(tweet, award):
+                    if ('actor' in tweet['text'].lower() and 'actor' not in award) or ('actress' in tweet['text'].lower() and 'actress' not in award) or ('tv' in tweet['text'].lower() and 'tv' not in award):
+                        continue
+
+                    # if award == 'best musical comedy':
+                    #     print(tweet['text'])
+
+                    tags = nltk.pos_tag(tknzr.tokenize(tweet["text"]))
+                    for i in range(len(tags)-1):
+                        first_tag = tags[i][1] == "NNP"
+                        first_word = tags[i][0].lower()
+                        sec_tag = tags[i+1][1] == "NNP"
+                        sec_word = tags[i+1][0].lower()
+
+                        if first_word in award_words or sec_word in award_words:
+                            continue
+
+                        # if not first_word.isalpha() or not sec_word.isalpha():
+                        #     continue
+
+                        if first_tag and first_word not in self.ignore and sec_tag and sec_word not in self.ignore:
+                            maybe_name = f"{first_word} {sec_word}"
+                            self.categorized_winners[award][maybe_name] += 1
+                            continue
+
+                        if first_tag and first_word not in self.ignore:
+                            unigrams[award][first_word] += 1
+                                
+        best_bn = None
+        best_bc = None
+        best_un = None
+        best_uc = None
+
+        for k,v in self.categorized_winners.items():
+            best_bn = float('-inf')
+            best_bc = None
+            for c, n in v.items():
+                if n > best_bn:
+                    best_bn = n
+                    best_bc = c
+
+            best_un = float('-inf')
+            best_uc = None
+            for uc, un in unigrams[k].items():
+                if un > best_un:
+                    best_un = un
+                    best_uc = uc
+
+            if (best_bn > 10 and best_un > 10 and abs(best_bn-best_un)/max(best_bn, best_un) < 0.525) or best_bn > best_un:
+                print(f"{best_bc} won {k} with {best_bn} occurrences")
+            elif best_un > best_bn:
+                print(f"{best_uc} won {k} with {best_un} occurrences")
+
+    def match_award(self, tweet, award):
+        for word in award.split():
+            if word not in tweet["text"].lower():
+                return False
+
+        return True
     
     def get_presenters(self, tweets): 
         present_count = {"presenters": {}, "present_plural": 0, "present_tweets": 0}
@@ -227,5 +239,5 @@ class GoldenGlobesParser:
             # print(text) 
 
 if __name__=="__main__":
-    dog = GoldenGlobesParser()
+    dog = GoldenGlobesParser(2013)
     dog.process_tweets()
